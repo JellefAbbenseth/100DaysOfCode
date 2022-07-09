6093cfa0ebd49616608360c474d686cfc5d5884a
Jellef Abbenseth <jellef@gmx.de>
2022-07-09 17:09:55 +0200

    100DaysOfCode
    - Day 14 - Project - Higher Lower Game

--- README.md
+++ README.md
@@ -26,4 +26,4 @@
 
 #### Source -- https://www.udemy.com/course/100-days-of-code/
 #### By Dr. Angela Yu -- https://www.udemy.com/user/4b4368a3-b5c8-4529-aa65-2056ec31f37e/
-#### Actual: Day 13 / 100 -- Chapter 142 / 695
+#### Actual: Day 14 / 100 -- Chapter 145 / 695
---
+++ Week_2/Day_14/Project/Day14_Project_higher-lower.py
@@ -0,0 +1,44 @@
+import random
+
+from game_data import data
+from art import *
+from replit import clear
+
+player_continues = True
+player_score = 0
+
+
+def get_random_data():
+    return random.choice(data)
+
+
+def print_star_data(star_data):
+    return f"{star_data['name']}, a {star_data['description']}, from {star_data['country']}."
+
+
+data_a = get_random_data()
+print(logo)
+
+while player_continues:
+    data_b = data_a
+    while data_b == data_a:
+        data_b = get_random_data()
+
+    print(f"Compare A: {print_star_data(data_a)}")
+    print(vs)
+    user_choice = input(
+        f"Against B: {print_star_data(data_b)}\nWho has more followers? "
+        f"Type 'A' or 'B': ").upper()
+
+    if user_choice == 'B' and data_b['follower_count'] > data_a['follower_count'] or \
+            user_choice == 'A' and data_a['follower_count'] > data_b['follower_count']:
+        player_score += 1
+        data_a = data_b
+        clear()
+        print(logo)
+        print(f"You're right! Current score: {player_score}.")
+    else:
+        player_continues = False
+        clear()
+        print(logo)
+        print(f"Sorry, that's wrong. final score: {player_score}.")
