0350fd40e764b72061f16d41c089b3f2a79e1347
Jellef Abbenseth <jellef@gmx.de>
2022-07-09 14:17:23 +0200

    100DaysOfCode
    - Day 12 - Project - Guess the number

--- README.md
+++ README.md
@@ -14,4 +14,4 @@
 
 #### Source -- https://www.udemy.com/course/100-days-of-code/
 #### By Dr. Angela Yu -- https://www.udemy.com/user/4b4368a3-b5c8-4529-aa65-2056ec31f37e/
-#### Actual: Day 11 / 100 -- Chapter 123 / 695
+#### Actual: Day 12 / 100 -- Chapter 131 / 695
---
+++ Week_2/Day_12/Project/Day12_Project_number-guessing.py
@@ -0,0 +1,36 @@
+# Guess a number game
+# difficulties attempts:
+#  - hard: 5
+#  - easy: 10
+import random
+
+from art import *
+
+print(logo)
+attempts = 10
+word = "attempts"
+won = False
+if input('Choose a difficulty. Type "easy" or "hard": ') == "hard":
+    attempts = 5
+
+secret_num = random.randint(0, 100)
+
+while attempts > 0 and won is False:
+    if attempts == 1:
+        word = "attempt"
+    print(f"You have {attempts} {word} remaining to guess the number.")
+    guess = int(input("Make a guess: "))
+
+    if guess == secret_num:
+        won = True
+    elif guess > secret_num:
+        print("Too high.")
+        attempts -= 1
+    else:
+        print("Too low.")
+        attempts -= 1
+
+if not won:
+    print(f"You've run out of guesses!\n{lose_logo}")
+else:
+    print(f"Congratulation you guessed correct!\n{win_logo}")
