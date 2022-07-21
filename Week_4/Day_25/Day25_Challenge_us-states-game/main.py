98c732101eedd7801c7347ecd441d3f1c086e2cc
Jellef Abbenseth <jellef@gmx.de>
2022-07-21 18:15:27 +0200

    100DaysOfCode
    - Day 25 - missing-states

--- Week_4/Day_25/Day25_Challenge_us-states-game/main.py
+++ Week_4/Day_25/Day25_Challenge_us-states-game/main.py
@@ -1,9 +1,15 @@
 import turtle
+
+import numpy
 import pandas
 from board import Board
 
 data_dict = pandas.read_csv("50_states.csv", delimiter=',').to_dict()
-print(data_dict)
+# print(data_dict)
+# data = pandas.read_csv("50_states.csv", delimiter=",")
+# data_list = data['state'].to_list()
+# print(data_list)
+# print(type(data_list))
 
 
 def check_input(state_name):
@@ -13,6 +19,14 @@ def check_input(state_name):
         return position
     else:
         return ""
+
+
+def save_missing_states(user_known_states):
+    csv_data = pandas.read_csv("50_states.csv", delimiter=",")
+    state_list = csv_data['state'].to_list()
+    missing_states = [i for i in state_list if i not in user_known_states]
+    new_data = pandas.DataFrame(missing_states)
+    new_data.to_csv("states_to_learn.csv")
 
 
 def capitalize_letters(message):
@@ -32,6 +46,7 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 
 turtle.shape(image)
+known_states = []
 
 while game_is_on:
     answer_state = screen.textinput(title=f"{board.count_states}/50 States Correct",
@@ -44,7 +59,12 @@ while game_is_on:
     if positions == "":
         continue
     board.write_state(positions, answer_state)
+    known_states.append(answer_state)
     if board.count_states >= 50:
         game_is_on = False
 
-
+if board.count_states >= 50:
+    print("Congratulation! You knew all states!")
+else:
+    print("So sad, you can still learn a bit more.")
+    save_missing_states(known_states)
