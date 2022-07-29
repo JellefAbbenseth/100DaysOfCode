77bcec07f0c555b8b3c462c3509bd0b5c729d0d0
Jellef Abbenseth <jellef@gmx.de>
2022-07-29 19:45:46 +0200

    100DaysOfCode
    Day 30 - Challenge - improving-password-generator

--- Week_5/Day_30/Challenge/Day30_Challenge_password-manager/main.py
+++ Week_5/Day_30/Challenge/Day30_Challenge_password-manager/main.py
@@ -4,6 +4,12 @@ from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
 
+# ---------------------------- CONSTANTS ------------------------------- #
+
+MAIL_ADDRESS = "test@test.com"
+EMAIL = "Email"
+PASSWORD = "Password:"
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -33,22 +39,55 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
     new_data = {website: {
-        "Email": email,
-        "Password": password,
+        EMAIL: email,
+        PASSWORD: password,
     }}
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't left any fields empty.")
     else:
-        with open("data.json", "r") as data_file:
-            data = json.load(data_file)
-            data.update(new_data)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+                data.update(new_data)
+        except FileNotFoundError:
+            data = new_data
 
         with open("data.json", "w") as data_file:
             json.dump(data, data_file, indent=4)
 
             website_entry.delete(0, END)
             password_entry.delete(0, END)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+def search():
+    website = website_entry.get()
+    if website == "":
+        messagebox.showwarning(title="No Website", message="No website given!")
+        return
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+            # It's better to check this exception with if else statement -> exception should be an exception !!
+            # user_data = data[website]
+            # messagebox.showinfo(title="User Data", message=f"Email: {user_data[EMAIL]}\n"
+            #                                                f"Password: {user_data[PASSWORD]}")
+    except FileNotFoundError:
+        messagebox.showerror(title="No File!", message="No Data File Found.")
+        return
+    # except KeyError:
+    #     messagebox.showerror(title="No Data!", message=f"No User Data for {website} found.\n"
+    #                                                      f"Please create a new Entry.")
+
+    if website in data:
+        user_data = data[website]
+        messagebox.showinfo(title="User Data", message=f"Email: {user_data[EMAIL]}\n"
+                                                       f"Password: {user_data[PASSWORD]}")
+    else:
+        messagebox.showwarning(title="No Data!", message=f"No User Data for {website} found.\n"
+                                                         f"Please create a new Entry.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -71,16 +110,18 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
-email_entry.insert(0, "angela@gmail.com")
+email_entry.insert(0, MAIL_ADDRESS)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text="Search", width=15, command=search)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=save)
