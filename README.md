e60b88c673dfb184cf89ea3b709b2981634a7e4b
Jellef Abbenseth <jellef@gmx.de>
2022-08-07 14:50:22 +0200

    100DaysOfCode
    Day 34 - Challenge - Quizzler

--- README.md
+++ README.md
@@ -30,4 +30,4 @@
 
 #### Source -- https://www.udemy.com/course/100-days-of-code/
 #### By Dr. Angela Yu -- https://www.udemy.com/user/4b4368a3-b5c8-4529-aa65-2056ec31f37e/
-#### Actual: Day 34 / 100 -- Chapter 322 / 695
+#### Actual: Day 34 / 100 -- Chapter 327 / 695
--- Week_5/Day_34/Challenges/Day34_Challenge_quizzler-app/ui.py
+++ Week_5/Day_34/Challenges/Day34_Challenge_quizzler-app/ui.py
@@ -1,39 +1,68 @@
 import tkinter as tk
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 FONT = ("Arial", 20, "italic")
 TRUE_IMG = "images/true.png"
 FALSE_IMG = "images/false.png"
 
-PADY = 20
-PADX = 20
+PAD_Y = 20
+PAD_X = 20
 
 
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = tk.Tk()
         self.window.title("Quizzler")
         self.score = 0
-        self.question = "Amazon qcquired Twitch in August 2014 for $970 million dollars."
-        self.window.config(padx=PADX, pady=PADY, bg=THEME_COLOR)
+        self.window.config(padx=PAD_X, pady=PAD_Y, bg=THEME_COLOR)
 
-        label = tk.Label(text=f"Score: {self.score}", bg=THEME_COLOR, fg="white")
-        label.grid(row=0, column=1)
+        self.label = tk.Label(text=f"Score: {self.score}", bg=THEME_COLOR, fg="white")
+        self.label.grid(row=0, column=1)
 
-        canvas = tk.Canvas(width=300, height=250)
-        self.question_text = canvas.create_text(150, 125, text=self.question, font=FONT, fill="black")
-        canvas.grid(row=1, column=0, columnspan=2, pady=PADY)
+        self.canvas = tk.Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="",
+            font=FONT,
+            fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=PAD_Y)
 
         true_img = tk.PhotoImage(file=TRUE_IMG)
-        true_button = tk.Button(image=true_img, highlightthickness=0, command=self.test)
-        true_button.grid(row=2, column=0)
+        self.true_button = tk.Button(image=true_img, highlightthickness=0, command=self.check_true)
+        self.true_button.grid(row=2, column=0)
         false_img = tk.PhotoImage(file=FALSE_IMG)
-        false_button = tk.Button(image=false_img, highlightthickness=0, command=self.test_two)
-        false_button.grid(row=2, column=1)
+        self.false_button = tk.Button(image=false_img, highlightthickness=0, command=self.check_false)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
         self.window.mainloop()
 
-    def test(self):
-        print("test")
+    def check_true(self):
+        self.give_feedback("True")
 
-    def test_two(self):
-        print("test 2")
+    def check_false(self):
+        self.give_feedback("False")
+
+    def give_feedback(self, answer: str):
+        if self.quiz.check_answer(answer):
+            self.score += 1
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(2000, self.get_next_question)
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.label.config(text=f"Score: {self.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz!")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
