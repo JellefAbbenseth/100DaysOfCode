82e4cf0424b624ed3a88ddbc60dd95695c471d3b
Jellef Abbenseth <jellef@gmx.de>
2022-08-09 18:25:23 +0200

    100DaysOfCode
    Day 35 - Challenge - hourly weather data - environment variables

--- Week_5/Day_35/Challenges/Day35_Challenge_hourly-weather-data/main.py
+++ Week_5/Day_35/Challenges/Day35_Challenge_hourly-weather-data/main.py
@@ -1,7 +1,8 @@
 # API_KEY only activated during the challenge
-import requests
+import os
 
-API_KEY = "b28794401acad3b900530ffb25ff0e92"
+import requests
+API_KEY = os.environ.get("OWN_API_KEY")
 MY_LAT = 52.513550
 MY_LONG = 13.387871
 RAINY_LAT = 46.607771388122394
@@ -26,10 +27,9 @@ def get_onecall_data():
     print(weather_data)
     twelve_hour_data = weather_data["hourly"][:12]
     print(twelve_hour_data)
-    for i in range(len(twelve_hour_data)):
-        for j in range(len(twelve_hour_data[i]["weather"])):
-            print(twelve_hour_data[i]["weather"][j]["id"])
-            if twelve_hour_data[i]["weather"][j]["id"] < 700:
+    for hour_data in twelve_hour_data:
+        for hourly_weather_data in hour_data["weather"]:
+            if int(hourly_weather_data["id"]) < 700:
                 print("Bring an Umbrella")
                 return
 
