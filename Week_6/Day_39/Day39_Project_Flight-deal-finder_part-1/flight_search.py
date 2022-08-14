8fc42c9a21eb9ade79282f1eaf3375835a912231
Jellef Abbenseth <jellef@gmx.de>
2022-08-14 16:59:54 +0200

    100DaysOfCode
    Day 39 - Project - Flight-deal-finder - part 2 - finished

--- Week_6/Day_39/Day39_Project_Flight-deal-finder_part-1/flight_search.py
+++ Week_6/Day_39/Day39_Project_Flight-deal-finder_part-1/flight_search.py
@@ -1,3 +1,26 @@
+import random
+
+
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
-    pass
+    # Class Search API needs private data, so I don't use it, will use random data
+    def __init__(self):
+        self.city_list = [
+            ["Paris", "PAR"],
+            ["Berlin", "BER"],
+            ["Tokyo", "TYO"],
+            ["Sydney", "SYD"],
+            ["Istanbul", "IST"],
+            ["Kuala Lumpur", "KUL"],
+            ["New York", "NYC"],
+            ["San Francisco", "SFO"],
+            ["Cape Town", "CPT"]
+        ]
+        self.random_price = []
+        self.lower_limit = 30
+        self.upper_limit = 600
+
+    def get_flight_prices(self):
+        for i in range(len(self.city_list)):
+            self.random_price.append(random.randint(self.lower_limit, self.upper_limit))
+        return self.random_price
--- Week_6/Day_39/Day39_Project_Flight-deal-finder_part-1/main.py
+++ Week_6/Day_39/Day39_Project_Flight-deal-finder_part-1/main.py
@@ -1,6 +1,28 @@
 # This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the
 # program requirements.
+import datetime
+import random
+
 from data_manager import DataManager
+from flight_search import FlightSearch
+
+
+def random_dates():
+    day = random.randint(1, 30)
+    month = random.randint(5, 12)
+    if (day + 14) > 30:
+        day_end = (day + 14) % 30
+        if month + 1 == 13:
+            month_end = 1
+        else:
+            month_end = month + 1
+    else:
+        day_end = day + 14
+    return [
+        datetime.datetime(year=2022, month=month, day=day).strftime("%Y-%m-%d"),
+        datetime.datetime(year=2022, month=month_end, day=day_end).strftime("%Y-%m-%d")
+    ]
+
 
 city_list = [
     ["Paris", "PAR"],
@@ -21,3 +43,13 @@ data_manager = DataManager()
 #     data_manager.update_data(city[0], city[1])
 # data_manager.save_updated_data()
 
+flight_search = FlightSearch()
+price_list = flight_search.get_flight_prices()
+data_list = data_manager.get_data()
+
+for i in range(len(price_list)):
+    if price_list[i] < data_list[i]["lowestPrice"]:
+        dates = random_dates()
+        print(f"Low price alert! Only ${price_list[i]} to fly from Franfurt to {data_list[i]['city']}, "
+              f"from {dates[0]} to {dates[1]}.")
+        break
