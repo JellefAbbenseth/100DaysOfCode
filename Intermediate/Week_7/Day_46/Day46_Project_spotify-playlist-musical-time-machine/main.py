0426a486ee073890168fbddda568a8d73b00e9a1
Jellef Abbenseth <jellef@gmx.de>
2022-08-24 20:18:39 +0200

    100DaysOfCode
    Day 46 - Project - Spotify musical time machine

--- Intermediate/Week_7/Day_46/Day46_Project_spotify-playlist-musical-time-machine/main.py
+++ Intermediate/Week_7/Day_46/Day46_Project_spotify-playlist-musical-time-machine/main.py
@@ -1,6 +1,5 @@
 import os
 import requests
-import lxml
 import spotipy as spotipy
 from bs4 import BeautifulSoup
 from spotipy.oauth2 import SpotifyOAuth
@@ -9,6 +8,10 @@ ID = os.environ["SPOTIFY_ID"]
 SECRET = os.environ["SPOTIFY_SECRET"]
 REDIRECT_URI = os.environ["SPOTIFY_REDIRECT"]
 URL = "https://www.billboard.com/charts/hot-100/2000-08-12/"
+SPOTIFY_O_AUTH = SpotifyOAuth(client_id=ID,
+                              client_secret=SECRET,
+                              redirect_uri=REDIRECT_URI,
+                              scope="playlist-modify-private")
 
 # date = input("Which year do you want to travel to? Type the date in this format: YYYY-MM-DD:\n")
 
@@ -20,15 +23,12 @@ soup = BeautifulSoup(billboard_web_page, "lxml")
 song_titles = [song.getText().strip() for song in soup.select(".o-chart-results-list__item #title-of-a-story")]
 print(song_titles)
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=ID,
-                                               client_secret=SECRET,
-                                               redirect_uri=REDIRECT_URI,
-                                               scope="playlist-modify-private"))
+sp = spotipy.Spotify(auth_manager=SPOTIFY_O_AUTH)
 
 results = sp.current_user()
 print(results)
 user_id = results["id"]
 print(user_id)
 
-results = sp.track(song_titles[0], id=user_id)
+results = sp.track(song_titles[0])
 print(results)
