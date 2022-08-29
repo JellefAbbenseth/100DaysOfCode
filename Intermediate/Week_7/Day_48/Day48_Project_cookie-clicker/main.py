ecf8d507d34dd5eb9d699df33caa8cb4f7bcf450
Jellef Abbenseth <jellef@gmx.de>
2022-08-29 19:01:35 +0200

    100DaysOfCode
    Day 48 - Project

--- Intermediate/Week_7/Day_48/Day48_Project_cookie-clicker/main.py
+++ Intermediate/Week_7/Day_48/Day48_Project_cookie-clicker/main.py
@@ -1,44 +1,41 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-DURATION = 0.5 * 60
+DURATION = 1 * 60
 DURATION_CHECK = 5
 
-chrome_driver_path = "/Users/Jellef/Documents/Programme/chromedriver_win32/chromedriver.exe"
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+# chrome_driver_path = "/Users/Jellef/Documents/Programme/chromedriver_win32/chromedriver.exe"
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)
+s = Service("/Users/Jellef/Documents/Programme/chromedriver_win32/chromedriver.exe")
+o = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=s, options=o)
 
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.ID, "cookie")
-store_items = driver.find_elements(By.CSS_SELECTOR, "#store div")
-prices = driver.find_elements(By.CSS_SELECTOR, "#store div b")
-price_data = []
-for i in range(8):
-    price_data.append(prices[i].text.split()[-1])
-
-
-# Todo: check code -> money is nearly zero every turn
-
-def buy_item():
-    global price_data, store_items
-    money_element = driver.find_element(By.ID, "money")
-    num_cookies = int(money_element.text)
-    print(f"Money = {num_cookies}")
-    for index in range(len(price_data) - 1):
-        if num_cookies < int(price_data[index]) and index > 0:
-            store_items[index].click()
-            return
-
 
 start_time = time.time()
+time_seconds = time.time() + 10
 cnt = 1
 while True:
-    if (start_time + DURATION) > (start_time + (cnt * DURATION_CHECK)):
-        buy_item()
-        print(f"Counter: {cnt}")
-        cnt += 1
     cookie.click()
+    if time.time() > time_seconds:
+        prices = driver.find_elements(By.CSS_SELECTOR, "#store div b")
+        price_data = []
+        for i in range(8):
+            price_data.append(int(prices[i].text.split()[-1].replace(",", "")))
+        cookie_count = int(driver.find_element(By.ID, 'money').text.replace(',', ''))
+        print(f"Money = {cookie_count}")
+        for index in range(len(price_data) - 1, -1, -1):
+            price = price_data[index]
+            if cookie_count > price:
+                store_items = driver.find_elements(By.CSS_SELECTOR, "#store div")
+                store_items[index].click()
+                break
+        time_seconds = time.time() + 10
+        cnt += 1
     if (start_time + DURATION) < time.time():
         break
 
