d610f3de417f0d24d6c7a4cc22f677a9212a8ed6
Jellef Abbenseth <jellef@gmx.de>
2022-09-07 19:54:06 +0200

    100DaysOfCode
    Day 52 - Project - Follower-bot

---
+++ Intermediate/Week_8/Day_52/Day52_Project_follower-bot/main.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+SPEED_TEST_URL = ""
+
+chrome_driver_path = "/Users/Jellef/Documents/Programme/chromedriver_win32/chromedriver.exe"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get(SPEED_TEST_URL)
+sleep(2.0)
