ce601fd17080ff0275aef180d8dbec2a7f1e7c53
Jellef Abbenseth <jellef@gmx.de>
2022-09-08 19:59:06 +0200

    100DaysOfCode
    Day 52 - Project - Follower-bot

--- Intermediate/Week_8/Day_52/Day52_Project_follower-bot/main.py
+++ Intermediate/Week_8/Day_52/Day52_Project_follower-bot/main.py
@@ -3,7 +3,7 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
+# Todo: Search fitting website
 SPEED_TEST_URL = ""
 
 chrome_driver_path = "/Users/Jellef/Documents/Programme/chromedriver_win32/chromedriver.exe"
