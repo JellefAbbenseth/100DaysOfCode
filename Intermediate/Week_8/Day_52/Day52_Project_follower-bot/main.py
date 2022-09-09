ce118125d042bfeab7be6a184f6d31c9e2fb56b4
Jellef Abbenseth <jellef@gmx.de>
2022-09-09 19:35:25 +0200

    100DaysOfCode
    Day 52 - Project - Follower-bot

--- Intermediate/Week_8/Day_52/Day52_Project_follower-bot/main.py
+++ Intermediate/Week_8/Day_52/Day52_Project_follower-bot/main.py
@@ -4,10 +4,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # Todo: Search fitting website
-SPEED_TEST_URL = ""
+ECOSIA_URL = "https://www.ecosia.org/?c=de"
 
 chrome_driver_path = "/Users/Jellef/Documents/Programme/chromedriver_win32/chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get(SPEED_TEST_URL)
+driver.get(ECOSIA_URL)
 sleep(2.0)
+
+search_field = driver.find_element(
+    By.CSS_SELECTOR,
+    "#__layout .search-form__input-wrapper input"
+)
+search_field.send_keys("Test")
+
+send_search = driver.find_element(
+    By.CSS_SELECTOR,
+    "#__layout .search-form-search-field .search-form__submit"
+)
+send_search.click()
