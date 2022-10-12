812681844c1d318410afe29c17f2a2fc9c33e376
Jellef Abbenseth <jellef@gmx.de>
2022-10-12 15:42:59 +0200

    100DaysOfCode
    Day 53 - Project - Data Entry Job Automation

--- Intermediate/Week_8/Day_53/Day53_Data-entry-job-automation/main.py
+++ Intermediate/Week_8/Day_53/Day53_Data-entry-job-automation/main.py
@@ -2,3 +2,9 @@
 # ToDo: update chrome_driver and check functionality
 # registration and checking information and store it
 # generate document and save it
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_driver_path = "/Users/Jellef/Documents/Programme/chromedriver_win32/chromedriver.exe"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
