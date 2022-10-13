2b2f1be23aecb7450f37228d51937343f123ebee
Jellef Abbenseth <jellef@gmx.de>
2022-10-13 16:16:31 +0200

    100DaysOfCode
    Day 53 - Project - Data Entry Job Automation

--- Intermediate/Week_8/Day_53/Day53_Data-entry-job-automation/main.py
+++ Intermediate/Week_8/Day_53/Day53_Data-entry-job-automation/main.py
@@ -3,8 +3,15 @@
 # registration and checking information and store it
 # generate document and save it
 
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+SITE_URL = "https://500px.com/popular"
+
 chrome_driver_path = "/Users/Jellef/Documents/Programme/chromedriver_win32/chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get(SITE_URL)
+sleep(5.0)
