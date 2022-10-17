b2924df25ca2f268abc7dc9e4f61baa01770d0f9
Jellef Abbenseth <jellef@gmx.de>
2022-10-17 07:39:06 +0200

    100DaysOfCode
    Day 53 - Project - Data Entry Job Automation

--- Intermediate/Week_8/Day_53/Day53_Data-entry-job-automation/main.py
+++ Intermediate/Week_8/Day_53/Day53_Data-entry-job-automation/main.py
@@ -1,17 +1,29 @@
 # Data Entry Job Automation
-# ToDo: update chrome_driver and check functionality
-# registration and checking information and store it
-# generate document and save it
 
 from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-SITE_URL = ""
+SITE_URL = "https://www.zillow.com/homes/for_rent/1-_beds/?searchQueryState=%7B%22pagination%22%3A%7B%7D%2C" \
+           "%22usersSearchTerm%22%3Anull%2C%22mapBounds%22%3A%7B%22west%22%3A-122.69219435644531%2C%22east%22%3A-122" \
+           ".17446364355469%2C%22south%22%3A37.703343724016136%2C%22north%22%3A37.847169233586946%7D%2C" \
+           "%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22fr%22%3A%7B%22value%22%3Atrue%7D%2C%22fsba%22%3A%7B" \
+           "%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C" \
+           "%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22fore%22%3A%7B%22value" \
+           "%22%3Afalse%7D%2C%22pmf%22%3A%7B%22value%22%3Afalse%7D%2C%22pf%22%3A%7B%22value%22%3Afalse%7D%2C%22mp%22" \
+           "%3A%7B%22max%22%3A3000%7D%2C%22price%22%3A%7B%22max%22%3A872627%7D%2C%22beds%22%3A%7B%22min%22%3A1%7D%7D" \
+           "%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A11%7D "
 
 chrome_driver_path = "/Users/Jellef/Documents/Programme/chromedriver_win32/chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get(SITE_URL)
-sleep(5.0)
+sleep(2.0)
+
+property_links = driver.find_elements(
+    By.LINK_TEXT,
+    "#grid-search-results ul .StyledPropertyCardPhotoBody-c11n-8-73-8__sc-128t811-0 a"
+)
+for link in property_links:
+    print(link.value_of_css_property('href'))
