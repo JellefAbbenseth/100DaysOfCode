4bba6249b9209c3aa6a1e1255b96bf69cf9e83c5
Jellef Abbenseth <jellef@gmx.de>
2022-11-03 08:44:18 +0100

    100DaysOfCode
    Day 63 - Challenge - Book Library

--- 03 Advanced/Week_10/Day_63/Challenges/Day63_Challenge_Book-library/server.py
+++ 03 Advanced/Week_10/Day_63/Challenges/Day63_Challenge_Book-library/server.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+Bootstrap(app)
+
+
+class EditForm(FlaskForm):
+    rating = StringField(label='', validators=[DataRequired()])
+    submit = SubmitField(label='Change Rating')
 
 
 class Book(db.Model):
@@ -21,33 +32,42 @@ class Book(db.Model):
 with app.app_context():
     db.create_all()
 
-all_books = []
+
+def load_books():
+    with app.app_context():
+        books = db.session.query(Book).all()
+    return books
 
 
-def add_book(title=str, author=str, rating=float):
-    all_books.append(
-        {
-            "title": title,
-            "author": author,
-            "rating": rating,
-        }
-    )
+def load_book_by_id(book_id):
+    with app.app_context():
+        book = db.session.query(Book).filter(Book.id == book_id)[0]
+        return book
+
+
+def add_book(title, author, rating=float):
     new_book = Book(title=title, author=author, rating=rating)
     db.session.add(new_book)
     db.session.commit()
 
 
-def load_books():
+def update_book_rating(book_id, new_rating):
     with app.app_context():
-        books = db.session.query(Book).all()
-    print(books[0])
+        book_to_update = Book.query.get(book_id)
+        book_to_update.rating = float(new_rating)
+        db.session.commit()
 
 
-load_books()
+def delete_book_by_id(book_id):
+    with app.app_context():
+        book_to_delete = Book.query.get(book_id)
+        db.session.delete(book_to_delete)
+        db.session.commit()
 
 
 @app.route('/')
 def home():
+    all_books = load_books()
     return render_template('index.html', book_list=all_books)
 
 
@@ -63,6 +83,23 @@ def add_data():
     rating = request.form['rating']
     add_book(title, author, float(rating))
 
+    return redirect(url_for('home'))
+
+
+@app.route("/edit/<id>", methods=['GET', 'POST'])
+def edit(id):
+    book = load_book_by_id(id)
+    form = EditForm()
+    if form.validate_on_submit():
+        new_rating = form.rating.data
+        update_book_rating(book.id, new_rating)
+        return redirect(url_for('home'))
+    return render_template("edit.html", form=form, book=book)
+
+
+@app.route("/delete/<id>")
+def delete(id):
+    delete_book_by_id(id)
     return redirect(url_for('home'))
 
 
