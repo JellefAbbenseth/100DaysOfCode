4dffde1984417600a920f9f1c7c31298f9ab6fb9
Jellef Abbenseth <jellef@gmx.de>
2022-11-04 13:49:54 +0100

    100DaysOfCode
    Day 66 - Challenge - Cafe-api

---
+++ 03 Advanced/Week_10/Day_66/Day66_Challenge_Cafe-api/server.py
@@ -0,0 +1,62 @@
+from random import choice
+
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# Cafe TABLE Configuration
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# HTTP GET - Read Record
+@app.route('/all')
+def get_all():
+    with app.app_context():
+        cafes = db.session.query(Cafe).all()
+    all_cafes = [cafe.to_dict() for cafe in cafes]
+    return jsonify(cafes=all_cafes)
+
+
+@app.route('/random')
+def get_random():
+    with app.app_context():
+        cafes = db.session.query(Cafe).all()
+    random_cafe = choice(cafes)
+    return jsonify(cafe=random_cafe.to_dict())
+
+
+# HTTP POST - Create Record
+
+# HTTP PUT/PATCH - Update Record
+
+# HTTP DELETE - Delete Record
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
