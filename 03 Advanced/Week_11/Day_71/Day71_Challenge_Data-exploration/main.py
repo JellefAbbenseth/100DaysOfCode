03f4dd35d3fab36ca2496d3ffd49f6c69c5f6aae
Jellef Abbenseth <jellef@gmx.de>
2023-02-28 10:49:33 +0100

    100DaysOfCode
    Day 71 - Exercise - Data Exploration

--- 03 Advanced/Week_11/Day_71/Day71_Challenge_Data-exploration/main.py
+++ 03 Advanced/Week_11/Day_71/Day71_Challenge_Data-exploration/main.py
@@ -30,3 +30,27 @@ print(clean_df.loc[lowest_starting_salary_id])
 print("\n2. Major with lowest mid-career salary:\n")
 lowest_mid_car_salary_id = clean_df['Mid-Career Median Salary'].idxmin()
 print(clean_df.loc[lowest_mid_car_salary_id])
+
+# differences
+print("\nIncluded Spread:\n")
+spread_col = clean_df['Mid-Career 90th Percentile Salary'] - clean_df['Mid-Career 10th Percentile Salary']
+clean_df.insert(1, 'Spread', spread_col)
+print(clean_df.head())
+
+print("\nLow risk:\n")
+low_risk = clean_df.sort_values('Spread')
+print(low_risk[['Undergraduate Major', 'Spread']].head())
+
+# 4. Highest potential and greatest spread
+print("\nHighest values:")
+high_value = clean_df.sort_values('Mid-Career 90th Percentile Salary', ascending=False)
+print(high_value[['Undergraduate Major', 'Mid-Career 90th Percentile Salary']].head(), end="\n\nHigh risk:\n")
+high_risk = clean_df.sort_values('Spread', ascending=False)
+print(high_risk[['Undergraduate Major', 'Spread']].head())
+
+# groupyby
+pd.options.display.float_format = '{:,.2f}'.format
+print("\nAverage Salary by group:")
+print(clean_df.groupby('Group').count())
+print("\nAverage salary by group:\n")
+print(clean_df.groupby('Group').mean())
