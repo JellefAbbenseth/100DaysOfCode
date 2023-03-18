2e053c96b85fd49b05909ee5efd1b33b034d09bb
Jellef Abbenseth <jellef@gmx.de>
2023-03-18 13:16:13 +0100

    100DaysOfCode
    Day 75 - Challenge - Beautiful Plotly Charts

--- 03 Advanced/Week_11/Day_75/Day75_Challenge_Plotly/main.py
+++ 03 Advanced/Week_11/Day_75/Day75_Challenge_Plotly/main.py
@@ -98,3 +98,134 @@ print(df_apps_clean.sort_values('Price', ascending=False).head(5))
 
 df_apps_clean['Revenue_Estimate'] = df_apps_clean.Installs.mul(df_apps_clean.Price)
 print(df_apps_clean.sort_values('Revenue_Estimate', ascending=False)[:10])
+
+# Plotly Bar Charts & Scatter Plots: Analysing App Categories
+
+print(df_apps_clean.Category.nunique())
+
+top10_category = df_apps_clean.Category.value_counts()[:10]
+print(top10_category)
+
+bar = px.bar(x = top10_category.index, y = top10_category.values)
+bar.show()
+
+# Horizontal Bar Chart - Most Popular Categories (Highest Downloads)
+
+category_installs = df_apps_clean.groupby('Category').agg({'Installs': pd.Series.sum})
+category_installs.sort_values('Installs', ascending=True, inplace=True)
+
+h_bar = px.bar(x = category_installs.Installs,
+               y = category_installs.index,
+               orientation='h',
+               title='Category Popularity')
+h_bar.update_layout(xaxis_title='Number of Downloads', yaxis_title='Category')
+h_bar.show()
+
+# Category Concentration - Downloads vs. Competition
+
+cat_number = df_apps_clean.groupby('Category').agg({'App': pd.Series.count})
+cat_merged_df = pd.merge(cat_number, category_installs, on='Category', how='inner')
+print(f'The dimensions of the DataFrame are: {cat_merged_df.shape}')
+cat_merged_df.sort_values('Installs', ascending=False)
+scatter = px.scatter(cat_merged_df,
+                     x='App',
+                     y='Installs',
+                     title='Category Concentration',
+                     size='App',
+                     hover_name=cat_merged_df.index,
+                     color='Installs')
+scatter.update_layout(xaxis_title="Number of Apps (Lower=More Concentrated)",
+                      yaxis_title="Installs",
+                      yaxis=dict(type='log'))
+scatter.show()
+
+# Extracting Nested Data from a Column
+
+print(len(df_apps_clean.Genres.unique()))
+
+print(df_apps_clean.Genres.value_counts().sort_values(ascending=True)[:5])
+
+stack = df_apps_clean.Genres.str.split(';', expand=True).stack()
+print(f'We now have a single column with shape: {stack.shape}')
+num_genres = stack.value_counts()
+print(f'Number of genres: {len(num_genres)}')
+
+# Colour Scales in Plotly Charts - Competition in Genres
+
+bar = px.bar(x = num_genres.index[:15],
+             y = num_genres.values[:15],
+             title='Top Genres',
+             hover_name=num_genres.index[:15],
+             color=num_genres.values[:15],
+             color_continuous_scale='Agsunset')
+bar.update_layout(xaxis_title='Genre',
+                  yaxis_title='Number of Apps',
+                  coloraxis_showscale=False)
+bar.show()
+
+# Grouped Bar Charts: Free vs. Paid Apps per Category
+
+print(df_apps_clean.Type.value_counts())
+
+df_free_vs_paid = df_apps_clean.groupby(["Category", "Type"], as_index=False).agg({'App': pd.Series.count})
+print(df_free_vs_paid.head())
+
+g_bar = px.bar(df_free_vs_paid,
+               x='Category',
+               y='App',
+               title='Free vs Paid Apps by Category',
+               color='Type',
+               barmode='group')
+
+g_bar.update_layout(xaxis_title='Category',
+                    yaxis_title='Number of Apps',
+                    xaxis={'categoryorder':'total descending'},
+                    yaxis=dict(type='log'))
+
+g_bar.show()
+
+# Plotly Box Plots: Lost Downloads for Paid Apps
+
+box = px.box(df_apps_clean,
+             y='Installs',
+             x='Type',
+             color='Type',
+             notched=True,
+             points='all',
+             title='How Many Downloads are Paid Apps Giving Up?')
+
+box.update_layout(yaxis=dict(type='log'))
+
+box.show()
+
+# Plotly Box Plots: Revenue by App Category
+
+df_paid_apps = df_apps_clean[df_apps_clean['Type'] == 'Paid']
+box = px.box(df_paid_apps,
+             x='Category',
+             y='Revenue_Estimate',
+             title='How Much Can Paid Apps Earn?')
+
+box.update_layout(xaxis_title='Category',
+                  yaxis_title='Paid App Ballpark Revenue',
+                  xaxis={'categoryorder':'min ascending'},
+                  yaxis=dict(type='log'))
+
+
+box.show()
+
+# How Much Can You Charge? Examine Paid App Pricing Strategies by Category
+
+print(df_paid_apps.Price.median())
+
+box = px.box(df_paid_apps,
+             x='Category',
+             y="Price",
+             title='Price per Category')
+
+box.update_layout(xaxis_title='Category',
+                  yaxis_title='Paid App Price',
+                  xaxis={'categoryorder':'max descending'},
+                  yaxis=dict(type='log'))
+
+box.show()
