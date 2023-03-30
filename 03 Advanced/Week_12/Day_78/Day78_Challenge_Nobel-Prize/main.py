96a494c24b23c9e1d7dd3076d2659983596144ab
Jellef Abbenseth <jellef@gmx.de>
2023-03-30 14:44:00 +0200

    100DaysOfCode
    Day 78 - Challenge - Nobel Prize

--- 03 Advanced/Week_12/Day_78/Day78_Challenge_Nobel-Prize/main.py
+++ 03 Advanced/Week_12/Day_78/Day78_Challenge_Nobel-Prize/main.py
@@ -93,3 +93,328 @@ v_bar_split = px.bar(x=cat_men_women.category,
 v_bar_split.update_layout(xaxis_title='Nobel Prize Category',
                           yaxis_title='Number of Prizes')
 v_bar_split.show()
+
+# Number of Prizes Awarded Over Time
+
+prize_per_year = df_data.groupby(by='year').count().prize
+print(prize_per_year)
+moving_average = prize_per_year.rolling(window=5).mean()
+print(moving_average)
+
+plt.scatter(x=prize_per_year.index,
+            y=prize_per_year.values,
+            c='dodgerblue',
+            alpha=0.7,
+            s=100,)
+plt.plot(prize_per_year.index,
+         moving_average.values,
+         c='crimson',
+         linewidth=3,)
+plt.show()
+
+plt.figure(figsize=(16,8), dpi=200)
+plt.title('Number of Nobel Prizes Awarded per Year', fontsize=18)
+plt.yticks(fontsize=14)
+plt.xticks(ticks=np.arange(1900, 2021, step=5),
+           fontsize=14,
+           rotation=45)
+
+ax = plt.gca()
+ax.set_xlim(1900, 2020)
+
+ax.scatter(x=prize_per_year.index,
+           y=prize_per_year.values,
+           c='dodgerblue',
+           alpha=0.7,
+           s=100,)
+
+ax.plot(prize_per_year.index,
+        moving_average.values,
+        c='crimson',
+        linewidth=3,)
+
+plt.show()
+
+# Are More Prizes Shared Than Before?
+
+yearly_avg_share = df_data.groupby(by='year').agg({'share_pct': pd.Series.mean})
+print(yearly_avg_share)
+share_moving_average = yearly_avg_share.rolling(window=5).mean()
+
+plt.figure(figsize=(16,8), dpi=200)
+plt.title('Number of Nobel Prizes Awarded per Year', fontsize=18)
+plt.yticks(fontsize=14)
+plt.xticks(ticks=np.arange(1900, 2021, step=5),
+           fontsize=14,
+           rotation=45)
+
+ax1 = plt.gca()
+ax2 = ax1.twinx()
+ax1.set_xlim(1900, 2020)
+
+ax1.scatter(x=prize_per_year.index,
+            y=prize_per_year.values,
+            c='dodgerblue',
+            alpha=0.7,
+            s=100,)
+
+ax1.plot(prize_per_year.index,
+         moving_average.values,
+         c='crimson',
+         linewidth=3,)
+
+ax2.plot(prize_per_year.index,
+         share_moving_average.values,
+         c='grey',
+         linewidth=3,)
+
+plt.show()
+
+plt.figure(figsize=(16,8), dpi=200)
+plt.title('Number of Nobel Prizes Awarded per Year', fontsize=18)
+plt.yticks(fontsize=14)
+plt.xticks(ticks=np.arange(1900, 2021, step=5),
+           fontsize=14,
+           rotation=45)
+
+ax1 = plt.gca()
+ax2 = ax1.twinx()
+ax1.set_xlim(1900, 2020)
+
+ax2.invert_yaxis()
+
+ax1.scatter(x=prize_per_year.index,
+            y=prize_per_year.values,
+            c='dodgerblue',
+            alpha=0.7,
+            s=100,)
+
+ax1.plot(prize_per_year.index,
+         moving_average.values,
+         c='crimson',
+         linewidth=3,)
+
+ax2.plot(prize_per_year.index,
+         share_moving_average.values,
+         c='grey',
+         linewidth=3,)
+
+plt.show()
+
+# The Countries with the Most Nobel Prizes
+
+top_countries = df_data.groupby(['birth_country_current'],
+                                as_index=False).agg({'prize': pd.Series.count})
+
+top_countries.sort_values(by='prize', inplace=True)
+top20_countries = top_countries[-20:]
+print(top20_countries)
+
+h_bar = px.bar(x=top20_countries.prize,
+               y=top20_countries.birth_country_current,
+               orientation='h',
+               color=top20_countries.prize,
+               color_continuous_scale='Viridis',
+               title='Top 20 Countries by Number of Prizes')
+
+h_bar.update_layout(xaxis_title='Number of Prizes',
+                    yaxis_title='Country',
+                    coloraxis_showscale=False)
+h_bar.show()
+
+# Use a Choropleth Map to Show the Number of Prizes Won by Country
+
+df_countries = df_data.groupby(['birth_country_current', 'ISO'],
+                               as_index=False).agg({'prize': pd.Series.count})
+print(df_countries.sort_values('prize', ascending=False))
+
+world_map = px.choropleth(df_countries,
+                          locations='ISO',
+                          color='prize',
+                          hover_name='birth_country_current',
+                          color_continuous_scale=px.colors.sequential.matter)
+
+world_map.update_layout(coloraxis_showscale=True,)
+
+world_map.show()
+
+# In Which Categories are the Different Countries Winning Prizes?
+
+cat_country = df_data.groupby(['birth_country_current', 'category'],
+                              as_index=False).agg({'prize': pd.Series.count})
+cat_country.sort_values(by='prize', ascending=False, inplace=True)
+
+merged_df = pd.merge(cat_country, top20_countries, on='birth_country_current')
+
+merged_df.columns = ['birth_country_current', 'category', 'cat_prize', 'total_prize']
+merged_df.sort_values(by='total_prize', inplace=True)
+
+cat_cntry_bar = px.bar(x=merged_df.cat_prize,
+                       y=merged_df.birth_country_current,
+                       color=merged_df.category,
+                       orientation='h',
+                       title='Top 20 Countries by Number of Prizes and Category')
+
+cat_cntry_bar.update_layout(xaxis_title='Number of Prizes',
+                            yaxis_title='Country')
+cat_cntry_bar.show()
+
+# Number of Prizes Won by Each Country Over Time
+
+prize_by_year = df_data.groupby(by=['birth_country_current', 'year'], as_index=False).count()
+prize_by_year = prize_by_year.sort_values('year')[['year', 'birth_country_current', 'prize']]
+
+cumulative_prizes = prize_by_year.groupby(by=['birth_country_current',
+                                              'year']).sum().groupby(level=[0]).cumsum()
+cumulative_prizes.reset_index(inplace=True)
+
+l_chart = px.line(cumulative_prizes,
+                  x='year',
+                  y='prize',
+                  color='birth_country_current',
+                  hover_name='birth_country_current')
+
+l_chart.update_layout(xaxis_title='Year',
+                      yaxis_title='Number of Prizes')
+
+l_chart.show()
+
+# What are the Top Research Organisations?
+
+top20_orgs = df_data.organization_name.value_counts()[:20]
+top20_orgs.sort_values(ascending=True, inplace=True)
+
+org_bar = px.bar(x = top20_orgs.values,
+                 y = top20_orgs.index,
+                 orientation='h',
+                 color=top20_orgs.values,
+                 color_continuous_scale=px.colors.sequential.haline,
+                 title='Top 20 Research Institutions by Number of Prizes')
+
+org_bar.update_layout(xaxis_title='Number of Prizes',
+                      yaxis_title='Institution',
+                      coloraxis_showscale=False)
+org_bar.show()
+
+# Which Cities Make the Most Discoveries?
+
+top20_org_cities = df_data.organization_city.value_counts()[:20]
+top20_org_cities.sort_values(ascending=True, inplace=True)
+city_bar2 = px.bar(x = top20_org_cities.values,
+                   y = top20_org_cities.index,
+                   orientation='h',
+                   color=top20_org_cities.values,
+                   color_continuous_scale=px.colors.sequential.Plasma,
+                   title='Which Cities Do the Most Research?')
+
+city_bar2.update_layout(xaxis_title='Number of Prizes',
+                        yaxis_title='City',
+                        coloraxis_showscale=False)
+city_bar2.show()
+
+# Where are Nobel Laureates Born? Chart the Laureate Birth Cities
+
+top20_cities = df_data.birth_city.value_counts()[:20]
+top20_cities.sort_values(ascending=True, inplace=True)
+city_bar = px.bar(x=top20_cities.values,
+                  y=top20_cities.index,
+                  orientation='h',
+                  color=top20_cities.values,
+                  color_continuous_scale=px.colors.sequential.Plasma,
+                  title='Where were the Nobel Laureates Born?')
+
+city_bar.update_layout(xaxis_title='Number of Prizes',
+                       yaxis_title='City of Birth',
+                       coloraxis_showscale=False)
+city_bar.show()
+
+# Plotly Sunburst Chart: Combine Country, City, and Organisation
+
+country_city_org = df_data.groupby(by=['organization_country',
+                                       'organization_city',
+                                       'organization_name'], as_index=False).agg({'prize': pd.Series.count})
+
+country_city_org = country_city_org.sort_values('prize', ascending=False)
+print(country_city_org)
+
+burst = px.sunburst(country_city_org,
+                    path=['organization_country', 'organization_city', 'organization_name'],
+                    values='prize',
+                    title='Where do Discoveries Take Place?',
+                    )
+
+burst.update_layout(xaxis_title='Number of Prizes',
+                    yaxis_title='City',
+                    coloraxis_showscale=False)
+
+burst.show()
+
+# Patterns in the Laureate Age at the Time of the Award
+
+birth_years = df_data.birth_date.dt.year
+df_data['winning_age'] = df_data.year - birth_years
+print(df_data)
+
+# Who were the oldest and youngest winners?
+
+# display(df_data.nlargest(n=1, columns='winning_age'))
+# display(df_data.nsmallest(n=1, columns='winning_age'))
+
+# Descriptive Statistics for the Laureate Age at Time of Award
+
+df_data.winning_age.describe()
+
+plt.figure(figsize=(8, 4), dpi=200)
+sns.histplot(data=df_data,
+             x=df_data.winning_age,
+             bins=30)
+plt.xlabel('Age')
+plt.title('Distribution of Age on Receipt of Prize')
+plt.show()
+
+# Age at Time of Award throughout History
+
+plt.figure(figsize=(8,4), dpi=200)
+with sns.axes_style("whitegrid"):
+    sns.regplot(data=df_data,
+                x='year',
+                y='winning_age',
+                lowess=True,
+                scatter_kws = {'alpha': 0.4},
+                line_kws={'color': 'black'})
+
+plt.show()
+
+# Winning Age Across the Nobel Prize Categories
+
+plt.figure(figsize=(8,4), dpi=200)
+with sns.axes_style("whitegrid"):
+    sns.boxplot(data=df_data,
+                x='category',
+                y='winning_age')
+
+plt.show()
+
+with sns.axes_style('whitegrid'):
+    sns.lmplot(data=df_data,
+               x='year',
+               y='winning_age',
+               row = 'category',
+               lowess=True,
+               aspect=2,
+               scatter_kws = {'alpha': 0.6},
+               line_kws = {'color': 'black'},)
+
+plt.show()
+
+with sns.axes_style("whitegrid"):
+    sns.lmplot(data=df_data,
+               x='year',
+               y='winning_age',
+               hue='category',
+               lowess=True,
+               aspect=2,
+               scatter_kws={'alpha': 0.5},
+               line_kws={'linewidth': 5})
+
+plt.show()
