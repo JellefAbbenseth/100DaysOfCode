c57d075135cfdee02858dd77a2eb70c2ed887998
Jellef Abbenseth <jellef@gmx.de>
2023-04-02 14:47:18 +0200

    100DaysOfCode
    Day 79 - Challenge - Cleaning

--- 03 Advanced/Week_12/Day_79/Day79_Challenge_Cleaning/main.py
+++ 03 Advanced/Week_12/Day_79/Day79_Challenge_Cleaning/main.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import scipy.stats as stats
 
 from pandas.plotting import register_matplotlib_converters
 
@@ -99,3 +100,172 @@ ax2.plot(df_monthly.date,
          linestyle='--')
 
 plt.show()
+
+# The Yearly Data Split by Clinic
+
+line = px.line(df_yearly,
+               x='year',
+               y='births',
+               color='clinic',
+               title='Total Yearly Births by Clinic')
+line.show()
+
+line = px.line(df_yearly,
+               x='year',
+               y='deaths',
+               color='clinic',
+               title='Total Yearly Deaths by Clinic')
+
+line.show()
+
+# Calculate the Proportion of Deaths at Each Clinic
+
+df_yearly['pct_deaths'] = df_yearly.deaths / df_yearly.births
+clinic_1 = df_yearly[df_yearly.clinic == 'clinic 1']
+avg_c1 = clinic_1.deaths.sum() / clinic_1.births.sum() * 100
+print(f'Average death rate in clinic 1 is {avg_c1:.3}%')
+
+clinic_2 = df_yearly[df_yearly.clinic == 'clinic 2']
+avg_c2 = clinic_2.deaths.sum() / clinic_2.births.sum() * 100
+print(f'Average death rate in clinic 2 is {avg_c2:.3}%')
+
+# Plotting the Proportion of Yearly Deaths by Clinic
+
+line = px.line(df_yearly,
+               x='year',
+               y='pct_deaths',
+               color='clinic',
+               title='Proportion of Yearly Deaths by Clinic')
+
+line.show()
+
+# The Effect of Handwashing
+
+handwashing_start = pd.to_datetime('1847-06-01')
+
+df_monthly['pct_deaths'] = df_monthly.deaths/df_monthly.births
+before_washing = df_monthly[df_monthly.date < handwashing_start]
+after_washing = df_monthly[df_monthly.date >= handwashing_start]
+
+bw_rate = before_washing.deaths.sum() / before_washing.births.sum() * 100
+aw_rate = after_washing.deaths.sum() / after_washing.births.sum() * 100
+print(f'Average death rate before 1847 was {bw_rate:.4}%')
+print(f'Average death rate AFTER 1847 was {aw_rate:.3}%')
+
+# Calculate a Rolling Average of the Death Rate
+
+roll_df = before_washing.set_index('date')
+roll_df = roll_df.rolling(window=6).mean()
+print(roll_df)
+
+# Highlighting Subsections of a Line Chart
+
+plt.figure(figsize=(14,8), dpi=200)
+plt.title('Percentage of Monthly Deaths over Time', fontsize=18)
+plt.yticks(fontsize=14)
+plt.xticks(fontsize=14, rotation=45)
+
+plt.ylabel('Percentage of Deaths', color='crimson', fontsize=18)
+
+ax = plt.gca()
+ax.xaxis.set_major_locator(years)
+ax.xaxis.set_major_formatter(years_fmt)
+ax.xaxis.set_minor_locator(months)
+ax.set_xlim([df_monthly.date.min(), df_monthly.date.max()])
+
+plt.grid(color='grey', linestyle='--')
+
+ma_line, = plt.plot(roll_df.index,
+                    roll_df.pct_deaths,
+                    color='crimson',
+                    linewidth=3,
+                    linestyle='--',
+                    label='6m Moving Average')
+bw_line, = plt.plot(before_washing.date,
+                    before_washing.pct_deaths,
+                    color='black',
+                    linewidth=1,
+                    linestyle='--',
+                    label='Before Handwashing')
+aw_line, = plt.plot(after_washing.date,
+                    after_washing.pct_deaths,
+                    color='skyblue',
+                    linewidth=3,
+                    marker='o',
+                    label='After Handwashing')
+
+plt.legend(handles=[ma_line, bw_line, aw_line],
+           fontsize=18)
+
+plt.show()
+
+# Statistics - Calculate the Difference in the Average Monthly Death Rate
+
+avg_prob_before = before_washing.pct_deaths.mean() * 100
+print(f'Chance of death during childbirth before handwashing: {avg_prob_before:.3}%.')
+
+avg_prob_after = after_washing.pct_deaths.mean() * 100
+print(f'Chance of death during childbirth AFTER handwashing: {avg_prob_after:.3}%.')
+
+mean_diff = avg_prob_before - avg_prob_after
+print(f'Handwashing reduced the monthly proportion of deaths by {mean_diff:.3}%!')
+
+times = avg_prob_before / avg_prob_after
+print(f'This is a {times:.2}x improvement!')
+
+# Use Box Plots to Show How the Death Rate Changed Before and After Handwashing
+
+df_monthly['washing_hands'] = np.where(df_monthly.date < handwashing_start, 'No', 'Yes')
+
+box = px.box(df_monthly,
+             x='washing_hands',
+             y='pct_deaths',
+             color='washing_hands',
+             title='How Have the Stats Changed with Handwashing?')
+
+box.update_layout(xaxis_title='Washing Hands?',
+                  yaxis_title='Percentage of Monthly Deaths',)
+
+box.show()
+
+# Use Histograms to Visualise the Monthly Distribution of Outcomes
+
+hist = px.histogram(df_monthly,
+                    x='pct_deaths',
+                    color='washing_hands',
+                    nbins=30,
+                    opacity=0.6,
+                    barmode='overlay',
+                    histnorm='percent',
+                    marginal='box',)
+
+hist.update_layout(xaxis_title='Proportion of Monthly Deaths',
+                   yaxis_title='Count',)
+
+hist.show()
+
+# Use a Kernel Density Estimate (KDE) to visualise a smooth distribution
+
+plt.figure(dpi=200)
+sns.kdeplot(before_washing.pct_deaths, shade=True)
+sns.kdeplot(after_washing.pct_deaths, shade=True)
+plt.title('Est. Distribution of Monthly Death Rate Before and After Handwashing')
+plt.show()
+
+plt.figure(dpi=200)
+sns.kdeplot(before_washing.pct_deaths,
+            shade=True,
+            clip=(0,1))
+sns.kdeplot(after_washing.pct_deaths,
+            shade=True,
+            clip=(0,1))
+plt.title('Est. Distribution of Monthly Death Rate Before and After Handwashing')
+plt.xlim(0, 0.40)
+plt.show()
+
+# Use a T-Test to Show Statistical Significance
+
+t_stat, p_value = stats.ttest_ind(a=before_washing.pct_deaths,
+                                  b=after_washing.pct_deaths)
+print(f'p-palue is {p_value:.10f}')
+print(f't-statstic is {t_stat:.4}')
