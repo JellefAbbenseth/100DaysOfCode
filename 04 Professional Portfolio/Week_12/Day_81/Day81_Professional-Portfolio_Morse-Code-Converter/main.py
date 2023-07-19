4f10e77336cc6cf5ed544233b1917221df92126d
Jellef Abbenseth <85156719+JellefAbbenseth@users.noreply.github.com>
2023-07-19 05:48:37 +0200

    100DaysOfCode
    Day 81 - Professional Portfolio Project - Text to Morse Code Converter

--- 04 Professional Portfolio/Week_12/Day_81/Day81_Professional-Portfolio_Morse-Code-Converter/main.py
+++ 04 Professional Portfolio/Week_12/Day_81/Day81_Professional-Portfolio_Morse-Code-Converter/main.py
@@ -21,8 +21,9 @@
 # special characters are changed or not needed
 
 code_dict = {
-    "a": "",
-    "b": ""
+    "a": "", "b": "", "c": "", "d": "", "e": "", "f": "", "g": "", "h": "", "i": "", "j": "", "k": "", "l": "",
+    "m": "", "n": "", "o": "", "p": "", "q": "", "r": "", "s": "", "t": "", "u": "", "v": "", "w": "", "x": "",
+    "y": "", "z": ""
 }
 
 print("Morse Code Converter")
