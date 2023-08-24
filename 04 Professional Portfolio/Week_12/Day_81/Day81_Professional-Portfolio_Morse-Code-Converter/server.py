13460e115d8901b8e341688713477ad311cc1fb2
Jellef Abbenseth <85156719+JellefAbbenseth@users.noreply.github.com>
2023-08-24 18:00:47 +0200

    100DaysOfCode
    Day 81 - Professional Portfolio Project - Text to Morse Code Converter

---
+++ 04 Professional Portfolio/Week_12/Day_81/Day81_Professional-Portfolio_Morse-Code-Converter/server.py
@@ -0,0 +1,13 @@
+from flask import app, render_template, Flask
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6dPZnzWlSihBXox7C0sKR6b'
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
