feb19e10c882c15b517d11e160d7b9a0fcb0c056
Jellef Abbenseth <85156719+JellefAbbenseth@users.noreply.github.com>
2023-09-01 22:06:35 +0200

    100DaysOfCode
    Day 81 - Professional Portfolio Project - Text to Morse Code Converter

--- 04 Professional Portfolio/Week_12/Day_81/Day81_Professional-Portfolio_Morse-Code-Converter/test.py
+++ 04 Professional Portfolio/Week_12/Day_81/Day81_Professional-Portfolio_Morse-Code-Converter/test.py
@@ -1,8 +1,5 @@
 from converter import Converter
 
-# Todo: testcases and tests
-# which cases are needed and which not?!
-# are there things to test for other parts?
 converter: Converter = Converter()
 
 print("Morse Code Converter")
